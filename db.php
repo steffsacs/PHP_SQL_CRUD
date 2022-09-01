b828f631b3334fd0739308dd1165bfecbc7d1daa
Stephanie Sanchez <steffsancor1996@gmail.com>
2022-08-31 20:57:18 -0500

    First commit db connection and index

---
+++ db.php
@@ -0,0 +1,15 @@
+<?php
+$conn= mysqli_connect(
+    'localhost',
+    'root',
+    '',
+    'crud_sql_php'
+);
+
+/* Check connection 
+if (isset($conn)){
+    echo 'DB is connected ';
+}
+
+*/
+?>
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php include ("db.php")
+/* Test index
+echo "Hello world"
+*/
+?>
+<?php include ("includes/header.php") ?>
+
+<h1> Hello World</h1>
+
+
+<!-- This are like traits -->    
+<?php include ("includes/footer.php") ?>
